65270000fad7261963b124f52d0c9b3550485f31
oberdev <qovalski@gmail.com>
2022-08-22 11:25:08 +0600

    init with empty project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+Hello there
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'modern-normalize/modern-normalize.css'
+import './assets/main.css'
+
+createApp(App).mount('#app')
